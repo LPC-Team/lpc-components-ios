2805a6cf46af4a5effc572dc726e827b52036550
Alaeddine Ouertani <alaeddine@lepotcommun.fr>
2018-08-07 16:23:06 +0200

    Add possibility to remove image and add phone number

--- lpc-components/Code/MailContact/Protocols/ContactActionsDelegate.swift
+++ lpc-components/Code/MailContact/Protocols/ContactActionsDelegate.swift
@@ -8,4 +8,5 @@
 
 public protocol ContactActionsDelegate: class {
     func onCopyMail()
+    func onErrorCallPhone()
 }
--- lpc-components/Code/MailContact/Views/EmailActionSheetHeader.swift
+++ lpc-components/Code/MailContact/Views/EmailActionSheetHeader.swift
@@ -20,10 +20,14 @@ final class EmailActionSheetHeader: UIView {
     // MARK: Properties
     
     static let viewHeight: CGFloat = 180
+    static let viewHeightWithoutImage: CGFloat = 113
     
     var image: UIImage? {
         didSet {
-            self.ibImageView.image = self.image
+            if self.ibImageView != nil {
+                self.ibImageView.image = self.image
+            }
+            self.initViews()
         }
     }
     
@@ -45,6 +49,14 @@ final class EmailActionSheetHeader: UIView {
         }
     }
     
+    var contactEmailColor: UIColor? {
+        didSet {
+            if let contactEmailColor = self.contactEmailColor {
+                self.ibEmailLabel.textColor = contactEmailColor
+            }
+        }
+    }
+    
     // MARK: Constructors
     
     required init?(coder aDecoder: NSCoder) {
@@ -53,32 +65,41 @@ final class EmailActionSheetHeader: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        let view = UIView()
-        view.frame = CGRect(x: 0, y: 0, width: frame.width - 30, height: frame.height)
-
-        self.ibImageView = UIImageView(frame: CGRect(x: 0, y: 20, width: 70, height: 52))
-        ibImageView.center.x = view.center.x
-        view.addSubview(self.ibImageView)
-        
-        self.ibTitleLabel = UILabel(frame: CGRect(x: 0, y: self.ibImageView.frame.maxY + 15, width: view.frame.width, height: 21))
-        self.ibTitleLabel.textAlignment = .center
-        self.ibTitleLabel.font = UIFont.systemFont(ofSize: 22, weight: .bold)
-        self.ibTitleLabel.textColor = UIColor(red: 67/255, green: 74/255, blue: 84/255, alpha: 1.0)
-        view.addSubview(self.ibTitleLabel)
-        
-        self.ibMessageLabel = UILabel(frame: CGRect(x: 0, y: self.ibTitleLabel.frame.maxY + 15, width: view.frame.width, height: 21))
-        self.ibMessageLabel.textAlignment = .center
-        self.ibMessageLabel.font = UIFont.systemFont(ofSize: 15)
-        self.ibMessageLabel.textColor = UIColor(red: 67/255, green: 74/255, blue: 84/255, alpha: 1.0)
-        view.addSubview(self.ibMessageLabel)
-        
-        self.ibEmailLabel = UILabel(frame: CGRect(x: 0, y: self.ibMessageLabel.frame.maxY + 2.5, width: view.frame.width, height: 21))
-        self.ibEmailLabel.textAlignment = .center
-        self.ibEmailLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
-        self.ibEmailLabel.textColor = UIColor(red: 226/255, green: 43/255, blue: 118/255, alpha: 1.0)
-        view.addSubview(self.ibEmailLabel)
-        
-        addSubview(view)
+    }
+    
+    // MARK: Private Methods
+    
+    private func initViews() {
+        if self.ibTitleLabel == nil {
+            let view = UIView()
+            view.frame = CGRect(x: 0, y: 0, width: frame.width - 30, height: frame.height)
+            
+            if self.image != nil {
+                self.ibImageView = UIImageView(frame: CGRect(x: 0, y: 20, width: 70, height: 52))
+                self.ibImageView.center.x = view.center.x
+                self.ibImageView.image = self.image
+                view.addSubview(self.ibImageView)
+            }
+            
+            self.ibTitleLabel = UILabel(frame: CGRect(x: 0, y: self.image != nil ? self.ibImageView.frame.maxY + 15 : 20, width: view.frame.width, height: 21))
+            self.ibTitleLabel.textAlignment = .center
+            self.ibTitleLabel.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+            self.ibTitleLabel.textColor = UIColor(red: 67/255, green: 74/255, blue: 84/255, alpha: 1.0)
+            view.addSubview(self.ibTitleLabel)
+            
+            self.ibMessageLabel = UILabel(frame: CGRect(x: 0, y: self.ibTitleLabel.frame.maxY + 15, width: view.frame.width, height: 21))
+            self.ibMessageLabel.textAlignment = .center
+            self.ibMessageLabel.font = UIFont.systemFont(ofSize: 15)
+            self.ibMessageLabel.textColor = UIColor(red: 67/255, green: 74/255, blue: 84/255, alpha: 1.0)
+            view.addSubview(self.ibMessageLabel)
+            
+            self.ibEmailLabel = UILabel(frame: CGRect(x: 0, y: self.ibMessageLabel.frame.maxY + 2.5, width: view.frame.width, height: 21))
+            self.ibEmailLabel.textAlignment = .center
+            self.ibEmailLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+            self.ibEmailLabel.textColor = UIColor(red: 226/255, green: 43/255, blue: 118/255, alpha: 1.0)
+            view.addSubview(self.ibEmailLabel)
+            
+            addSubview(view)
+        }
     }
 }
