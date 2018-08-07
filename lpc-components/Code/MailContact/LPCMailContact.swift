04009e1c7345d4835c57a112947bc39eb2db8180
Alaeddine Ouertani <alaeddine@lepotcommun.fr>
2018-08-07 16:47:36 +0200

    Fix the lPCMailContact compilation

--- lpc-components/Code/MailContact/LPCMailContact.swift
+++ lpc-components/Code/MailContact/LPCMailContact.swift
@@ -15,13 +15,15 @@ public final class LPCMailContact {
     
     let viewController: UIViewController!
     let sender: UIView?
-    let image: UIImage!
+    let image: UIImage?
     let title: String!
     let message: String!
     let contactEmail: String!
     let openText: String!
     let copyText: String!
     let cancelText: String!
+    let phoneNumber: String?
+    let contactEmailColor: UIColor?
     let mailComposeDelegate: MFMailComposeViewControllerDelegate!
     let contactActionsDelegate: ContactActionsDelegate!
     
@@ -29,13 +31,15 @@ public final class LPCMailContact {
     
     public init(_ viewController: UIViewController,
                 sender: UIView? = nil,
-                image: UIImage,
+                image: UIImage? = nil,
                 title: String,
                 message: String,
                 contactEmail: String,
                 openText: String,
                 copyText: String,
                 cancelText: String,
+                phoneNumber: String? = nil,
+                contactEmailColor: UIColor? = nil,
                 mailComposeDelegate: MFMailComposeViewControllerDelegate,
                 contactActionsDelegate: ContactActionsDelegate) {
         self.viewController = viewController
@@ -47,19 +51,23 @@ public final class LPCMailContact {
         self.openText = openText
         self.copyText = copyText
         self.cancelText = cancelText
+        self.phoneNumber = phoneNumber
+        self.contactEmailColor = contactEmailColor
         self.mailComposeDelegate = mailComposeDelegate
         self.contactActionsDelegate = contactActionsDelegate
     }
     
     public convenience init(_ viewController: UIViewController,
                             sender: UIView? = nil,
-                            image: UIImage,
+                            image: UIImage? = nil,
                             title: String,
                             message: String,
                             contactEmail: String,
                             openText: String,
                             copyText: String,
-                            cancelText: String) {
+                            cancelText: String,
+                            phoneNumber: String? = nil,
+                            contactEmailColor: UIColor? = nil) {
         guard let mailComposeDelegate = viewController as? MFMailComposeViewControllerDelegate else {
             fatalError("Your viewController \(viewController) must implement MFMailComposeViewControllerDelegate")
         }
@@ -68,21 +76,38 @@ public final class LPCMailContact {
             fatalError("Your viewController \(viewController) must implement ContactActionsDelegate")
         }
         
-        self.init(viewController, sender: sender, image: image, title: title, message: message, contactEmail: contactEmail, openText: openText, copyText: copyText, cancelText: cancelText, mailComposeDelegate: mailComposeDelegate, contactActionsDelegate: contactActionsDelegate)
+        self.init(viewController,
+                  sender: sender,
+                  image: image,
+                  title: title,
+                  message: message,
+                  contactEmail: contactEmail,
+                  openText: openText,
+                  copyText: copyText,
+                  cancelText: cancelText,
+                  phoneNumber: phoneNumber,
+                  contactEmailColor: contactEmailColor,
+                  mailComposeDelegate: mailComposeDelegate,
+                  contactActionsDelegate: contactActionsDelegate)
     }
     
     // MARK: Public Methods
     
     public final func show(isIPad: Bool = false) {
         let actionSheetController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let headerHeight = self.image == nil ? EmailActionSheetHeader.viewHeightWithoutImage : EmailActionSheetHeader.viewHeight
         if isIPad {
-            actionSheetController.prepareHeader(headerHeight: EmailActionSheetHeader.viewHeight)
+            actionSheetController.prepareHeader(headerHeight: headerHeight)
         } else {
-            let header = EmailActionSheetHeader(frame: CGRect(x: 0, y: 0, width: actionSheetController.view.bounds.size.width, height: EmailActionSheetHeader.viewHeight))
+            let header = EmailActionSheetHeader(frame: CGRect(x: 0,
+                                                              y: 0,
+                                                              width: actionSheetController.view.bounds.size.width,
+                                                              height: headerHeight))
             header.image = self.image
             header.title = self.title
             header.message = self.message
             header.contactEmail = self.contactEmail
+            header.contactEmailColor = self.contactEmailColor
             actionSheetController.addHeader(header)
         }
         
@@ -107,6 +132,21 @@ public final class LPCMailContact {
         }
         actionSheetController.addAction(copyAdressActionButton)
         
+        if let phoneNumber = self.phoneNumber {
+            let phoneButton = UIAlertAction(title: phoneNumber, style: .default) { _ -> Void in
+                if let url = URL(string: "tel://\(phoneNumber.components(separatedBy: .whitespaces).joined())"), UIApplication.shared.canOpenURL(url) {
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    } else {
+                        UIApplication.shared.openURL(url)
+                    }
+                } else {
+                    self.contactActionsDelegate.onErrorCallPhone()
+                }
+            }
+            actionSheetController.addAction(phoneButton)
+        }
+        
         if let popoverController = actionSheetController.popoverPresentationController, let sender = self.sender {
             popoverController.sourceView = sender
             popoverController.sourceRect = CGRect(x: sender.bounds.midX, y: sender.bounds.midY, width: 0, height: 0)
@@ -114,7 +154,8 @@ public final class LPCMailContact {
         
         self.viewController.present(actionSheetController, animated: true, completion: {
             if isIPad {
-                let header = EmailActionSheetHeader(frame: CGRect(x: 15, y: 0, width: actionSheetController.view.bounds.size.width, height: EmailActionSheetHeader.viewHeight))
+                let headerHeight = self.image == nil ? EmailActionSheetHeader.viewHeightWithoutImage : EmailActionSheetHeader.viewHeight
+                let header = EmailActionSheetHeader(frame: CGRect(x: 15, y: 0, width: actionSheetController.view.bounds.size.width, height: headerHeight))
                 header.image = self.image
                 header.title = self.title
                 header.message = self.message
